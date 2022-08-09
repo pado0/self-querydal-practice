e6fe388c3293487819631cb5aadfd9cc08cecfa0
hyo321 <hyo321@lguplus.co.kr>
2022-08-09 21:44:55 +0900

    Initial commit

---
+++ src/main/kotlin/com/pado/selfquerydalpractice/SelfQuerydalPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.pado.selfquerydalpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SelfQuerydalPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SelfQuerydalPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/pado/selfquerydalpractice/SelfQuerydalPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pado.selfquerydalpractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SelfQuerydalPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
