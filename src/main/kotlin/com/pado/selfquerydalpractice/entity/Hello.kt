aa8f61c8bbf4efd0045d1b2814825e4a7bdf1b99
hyo321 <hyo321@lguplus.co.kr>
2022-08-10 22:46:00 +0900

    effective kotlin item 43, 44

---
+++ src/main/kotlin/com/pado/selfquerydalpractice/entity/Hello.kt
@@ -0,0 +1,11 @@
+package com.pado.selfquerydalpractice.entity
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "hello_table")
+class Hello(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    val greeting: String,
+)
